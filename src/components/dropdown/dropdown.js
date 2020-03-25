5b3362e6da97d3d55d064054f625b47931b1df7b
maksim1509 <maaa23@mail.ru>
2020-03-25 14:10:17 +0400

    setting dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,40 +1,27 @@
-const dropdownControler = () => {
-    const arrowState = {
-        down: true,
-    }
-    const dropdownColl = document.querySelectorAll('.dropdown');
-    dropdownColl.forEach((dd) => {
-        const field = dd.querySelector('.dropdown__field');
-        const dropbox = dd.querySelector('.dropdown__dropbox');
-        const arrow = dd.querySelector('img');
-        field.addEventListener('click', (e) => {
+const dropdownController = () => {   
+    const dropdownColl = document.querySelectorAll('.dropdown__field');
+
+    dropdownColl.forEach((dropdown) => {
+        dropdown.addEventListener('click', ({ target }) => {
+            const nameField = target.querySelector('.dropdown__field-name');
+            const arrow = target.querySelector('img');
+            const dropbox = target.nextElementSibling;
             dropbox.classList.toggle('dropdonw__dropbox--show');
             arrow.classList.toggle('dropdown__arrow--reverse');
-            e.target.scrollIntoView();
-        })
-    })
-    
-    const titleFieldColl = document.querySelectorAll('.dropdown__dropbox-title');
-    titleFieldColl.forEach((title) => {
-        title.addEventListener('click', (e) => {
-            const list = title.querySelector('.dropdown__dropbox-content');     
-            list.classList.toggle('dropdonw__dropbox--show');
-            list.addEventListener('click', (e) => {
-                const selectItem = e.target.textContent;
-                const nameField = item.closest('.dropdown').querySelector('.dropdown__field-name');
-                nameField.textContent = selectItem;
-            })
-        });
-    });
-    
-    // const content = document.querySelectorAll('.dropdown__dropbox-content');
-    // content.forEach((item) => {
-    //     item.addEventListener('click', (e) => {
-    //         const selectItem = e.target.textContent;
-    //         const nameField = item.closest('.dropdown').querySelector('.dropdown__field-name');
-    //         nameField.textContent = selectItem;
-    //     })
-    // })
-}
 
-dropdownControler();
+            const dropboxTitleColl = dropbox.querySelectorAll('.dropdown__dropbox-title');
+
+            dropboxTitleColl.forEach((dropboxTitle) => {
+                dropboxTitle.addEventListener('click', (e) => {
+                    const dropboxContent = e.target.querySelector('.dropdown__dropbox-content');
+                    dropboxContent.addEventListener('click', (e) => {
+                        nameField.textContent = e.target.textContent;
+                        e.stopPropagation();
+                    });
+                    dropboxContent.classList.toggle('dropdonw__dropbox--show');
+                });
+            });
+        });
+    });   
+};
+dropdownController();
