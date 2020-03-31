3822d62ccb02603893b6ac0592a26c85c0e2a796
maksim1509 <maaa23@mail.ru>
2020-03-31 11:06:06 +0400

    add types travels to dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -4,7 +4,6 @@ const dropdownController = () => {
     dropdownColl.forEach((dropdown) => {
         dropdown.addEventListener('click', ({ target }) => {
             const currentDropdown = target.closest('.dropdown');
-            console.log(currentDropdown);
             const nameField = currentDropdown.querySelector('.dropdown__field-name');
             const arrow = currentDropdown.querySelector('img');
             const dropbox = currentDropdown.querySelector('.dropdown__dropbox');
@@ -13,16 +12,25 @@ const dropdownController = () => {
 
             const dropboxTitleColl = dropbox.querySelectorAll('.dropdown__dropbox-title');
 
-            dropboxTitleColl.forEach((dropboxTitle) => {
-                dropboxTitle.addEventListener('click', (e) => {
-                    const dropboxContent = e.target.querySelector('.dropdown__dropbox-content');
-                    dropboxContent.classList.toggle('dropdown__dropbox--show');
-                    dropboxContent.addEventListener('click', (e) => {
-                        nameField.textContent = e.target.textContent;
-                        e.stopPropagation();
+            if (dropbox.querySelector('.dropdown__dropbox-content')) {
+                dropboxTitleColl.forEach((dropboxTitle) => {
+                    dropboxTitle.addEventListener('click', (e) => {
+                        const dropboxContent = e.target.querySelector('.dropdown__dropbox-content');
+                        dropboxContent.classList.toggle('dropdown__dropbox--show');
+                        dropboxContent.addEventListener('click', (e) => {
+                            nameField.textContent = e.target.textContent;
+                            e.stopPropagation();
+                        });
                     });
                 });
-            });
+            } else {
+                const dropboxTitles = dropbox.querySelectorAll('.dropdown__dropbox-title--short');
+                dropboxTitles.forEach((title) => {
+                    title.addEventListener('click', ({ target }) => {
+                        nameField.textContent = target.textContent;
+                    });
+                });
+            }
         });
     });
 };
